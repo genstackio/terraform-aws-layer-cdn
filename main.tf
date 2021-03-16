0fe21a72581255377980751e2223ce181010198b
Olivier Hoareau <oha@greenberets.io>
2021-03-16 18:14:41 +0100

    fix: fix

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "cdn" {
   source         = "genstackio/website/aws//modules/cdn"
-  version        = "0.1.10"
+  version        = "0.1.11"
   name           = var.name
   origin         = var.origin
   zone           = var.dns_zone
