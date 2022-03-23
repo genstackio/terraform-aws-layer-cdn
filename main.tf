15e6fb685370f1726f3c1390f865c4e1ba0d11f3
Olivier Hoareau <oha@greenberets.io>
2022-03-23 12:27:37 +0100

    feat: enhancements

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "cdn" {
   source               = "genstackio/website/aws//modules/cdn"
-  version              = "0.1.12"
+  version              = "0.2.2"
   name                 = var.name
   origin               = var.origin
   zone                 = var.dns_zone
@@ -8,6 +8,18 @@ module "cdn" {
   apex_redirect        = var.apex_redirect
   forward_query_string = var.forward_query_string
   geolocations         = var.geolocations
+  price_class          = var.price_class
+  headers              = var.headers
+  keep_host            = var.keep_host
+  forward_headers      = var.forwarded_headers
+  default_root         = var.default_root
+  index_document       = var.index_document
+  error_document       = var.error_document
+  error_403_page_path  = var.error_403_page_path
+  error_404_page_path  = var.error_404_page_path
+  functions            = var.functions
+  edge_lambdas         = var.edge_lambdas
+  edge_lambdas_variables = var.edge_lambdas_variables
   providers            = {
     aws     = aws
     aws.acm = aws.acm
--- variables.tf
+++ variables.tf
@@ -23,3 +23,67 @@ variable "forward_query_string" {
 variable "dns_zone" {
   type = string
 }
+variable "headers" {
+  type    = map(string)
+  default = {}
+}
+variable "keep_host" {
+  type    = bool
+  default = true
+}
+variable "forwarded_headers" {
+  type    = list(string)
+  default = ["*"]
+}
+variable "default_root" {
+  type    = string
+  default = null
+}
+variable "index_document" {
+  type    = string
+  default = "index.html"
+}
+variable "error_document" {
+  type    = string
+  default = ""
+}
+variable "error_403_page_path" {
+  type    = string
+  default = ""
+}
+variable "error_404_page_path" {
+  type    = string
+  default = ""
+}
+variable "price_class" {
+  type    = string
+  default = "PriceClass_100"
+}
+variable "lambdas" {
+  type = list(object({
+    event_type = string
+    lambda_arn = string
+    include_body = bool
+  }))
+  default = []
+}
+variable "functions" {
+  type = list(object({
+    name = string
+    event_type = string
+    code = string
+  }))
+}
+variable "edge_lambdas" {
+  type = list(object({
+    event_type = string
+    lambda_arn = string
+    include_body = bool
+  }))
+  default = []
+}
+variable "edge_lambdas_variables" {
+  type    = map(string)
+  default = {}
+}
+
