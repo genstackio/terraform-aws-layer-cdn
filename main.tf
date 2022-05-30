63cf380ee599dd1ce9e05715ce5c70600f6b3414
Olivier Hoareau <oha@greenberets.io>
2022-05-30 12:06:32 +0200

    chore: up to compatibility to tf 1.2.1 + aws provider 4.16.0

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "cdn" {
   source               = "genstackio/website/aws//modules/cdn"
-  version              = "0.3.2"
+  version              = "0.3.3"
   name                 = var.name
   origin               = var.origin
   zone                 = var.dns_zone
